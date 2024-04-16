41333db0037425e0cb2a57be26b81ccd6f517d69
Rosentus <reece.smith2@mail.bcu.ac.uk>
2024-04-16 13:53:43 +0100

    Playtesting
    Simple playtesting of the different mechanics, added a few small things and adjusted different variables for the tank tracks, propellor and cube core (cube core being weight adjusted)

--- projProblox/Intermediate/Build/Win64/UnrealEditor/Inc/projProblox/UHT/projProblox.init.gen.cpp
+++ projProblox/Intermediate/Build/Win64/UnrealEditor/Inc/projProblox/UHT/projProblox.init.gen.cpp
@@ -35,7 +35,7 @@ void EmptyLinkFunctionForGeneratedCodeprojProblox_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x806C21ED,
+				0x39AEEB8A,
 				0x5DF15AE1,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -43,5 +43,5 @@ void EmptyLinkFunctionForGeneratedCodeprojProblox_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_projProblox.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_projProblox(Z_Construct_UPackage__Script_projProblox, TEXT("/Script/projProblox"), Z_Registration_Info_UPackage__Script_projProblox, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x806C21ED, 0x5DF15AE1));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_projProblox(Z_Construct_UPackage__Script_projProblox, TEXT("/Script/projProblox"), Z_Registration_Info_UPackage__Script_projProblox, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x39AEEB8A, 0x5DF15AE1));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
