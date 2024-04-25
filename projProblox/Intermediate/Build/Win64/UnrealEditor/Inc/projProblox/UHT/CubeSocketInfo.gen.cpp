f5266a61bc78b295d1f794af793f9ae2f5d65752
Rosentus <reece.smith2@mail.bcu.ac.uk>
2024-04-25 16:56:00 +0100

    made menus work
    added the code and such to make all the UI link and load levels as well as a pause menu to exit out of the level

--- projProblox/Intermediate/Build/Win64/UnrealEditor/Inc/projProblox/UHT/CubeSocketInfo.gen.cpp
+++ projProblox/Intermediate/Build/Win64/UnrealEditor/Inc/projProblox/UHT/CubeSocketInfo.gen.cpp
@@ -111,15 +111,15 @@ void EmptyLinkFunctionForGeneratedCodeCubeSocketInfo() {}
 	UCubeSocketInfo::UCubeSocketInfo(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {}
 	DEFINE_VTABLE_PTR_HELPER_CTOR(UCubeSocketInfo);
 	UCubeSocketInfo::~UCubeSocketInfo() {}
-	struct Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_CubeSocketInfo_h_Statics
+	struct Z_CompiledInDeferFile_FID_GitHub_FMP_problox_projProblox_Source_projProblox_CubeSocketInfo_h_Statics
 	{
 		static const FClassRegisterCompiledInInfo ClassInfo[];
 	};
-	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_CubeSocketInfo_h_Statics::ClassInfo[] = {
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_GitHub_FMP_problox_projProblox_Source_projProblox_CubeSocketInfo_h_Statics::ClassInfo[] = {
 		{ Z_Construct_UClass_UCubeSocketInfo, UCubeSocketInfo::StaticClass, TEXT("UCubeSocketInfo"), &Z_Registration_Info_UClass_UCubeSocketInfo, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UCubeSocketInfo), 4273995624U) },
 	};
-	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_CubeSocketInfo_h_935482122(TEXT("/Script/projProblox"),
-		Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_CubeSocketInfo_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_CubeSocketInfo_h_Statics::ClassInfo),
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_GitHub_FMP_problox_projProblox_Source_projProblox_CubeSocketInfo_h_935482122(TEXT("/Script/projProblox"),
+		Z_CompiledInDeferFile_FID_GitHub_FMP_problox_projProblox_Source_projProblox_CubeSocketInfo_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_GitHub_FMP_problox_projProblox_Source_projProblox_CubeSocketInfo_h_Statics::ClassInfo),
 		nullptr, 0,
 		nullptr, 0);
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- projProblox/Intermediate/Build/Win64/UnrealEditor/Inc/projProblox/UHT/ThingSpawner.gen.cpp
+++ projProblox/Intermediate/Build/Win64/UnrealEditor/Inc/projProblox/UHT/ThingSpawner.gen.cpp
@@ -108,15 +108,15 @@ void EmptyLinkFunctionForGeneratedCodeThingSpawner() {}
 	}
 	DEFINE_VTABLE_PTR_HELPER_CTOR(AThingSpawner);
 	AThingSpawner::~AThingSpawner() {}
-	struct Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_ThingSpawner_h_Statics
+	struct Z_CompiledInDeferFile_FID_GitHub_FMP_problox_projProblox_Source_projProblox_ThingSpawner_h_Statics
 	{
 		static const FClassRegisterCompiledInInfo ClassInfo[];
 	};
-	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_ThingSpawner_h_Statics::ClassInfo[] = {
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_GitHub_FMP_problox_projProblox_Source_projProblox_ThingSpawner_h_Statics::ClassInfo[] = {
 		{ Z_Construct_UClass_AThingSpawner, AThingSpawner::StaticClass, TEXT("AThingSpawner"), &Z_Registration_Info_UClass_AThingSpawner, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AThingSpawner), 3022071091U) },
 	};
-	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_ThingSpawner_h_1716086497(TEXT("/Script/projProblox"),
-		Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_ThingSpawner_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_ThingSpawner_h_Statics::ClassInfo),
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_GitHub_FMP_problox_projProblox_Source_projProblox_ThingSpawner_h_1716086497(TEXT("/Script/projProblox"),
+		Z_CompiledInDeferFile_FID_GitHub_FMP_problox_projProblox_Source_projProblox_ThingSpawner_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_GitHub_FMP_problox_projProblox_Source_projProblox_ThingSpawner_h_Statics::ClassInfo),
 		nullptr, 0,
 		nullptr, 0);
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
