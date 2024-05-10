a06f6a552db4ff35724794a37fe08b0b3d92151a
keddah <deanatkinsonwalker@gmail.com>
2024-05-10 14:41:22 +0100

    balloons with string

--- projProblox/Source/projProblox/Pickupables/Balloon.cpp
+++ projProblox/Source/projProblox/Pickupables/Balloon.cpp
@@ -6,15 +6,116 @@
 
 #include "Cores/CubeCore.h"
 
+ABalloon::ABalloon()
+{
+	string = CreateDefaultSubobject<UCableComponent>("String");
+	string->SetupAttachment(objMesh);
+
+	constraint = CreateDefaultSubobject<UPhysicsConstraintComponent>("Constraint");
+	constraint->SetupAttachment(string);
+
+	string->bAttachEnd = false;
+	string->CableLength = 200;
+	string->NumSegments = 16;
+	string->NumSides = 4;
+	string->SolverIterations = 2;
+	string->EndLocation = {};
+
+	constraint->SetLinearXLimit(LCM_Limited, string->CableLength);
+	constraint->SetLinearYLimit(LCM_Limited, string->CableLength);
+	constraint->SetLinearZLimit(LCM_Limited, string->CableLength);
+}
+
 void ABalloon::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 
+	SetConstraintsActive(!(selected || groupSelected));
+}
+
+void ABalloon::Ability(float deltaTime)
+{
+	Super::Ability(deltaTime);
+
 	if(!parentCore || !isAttached) return;
+	
 	FVector velocity = objMesh->GetPhysicsLinearVelocity();
-	velocity.Z *= -DeltaSeconds;
+	velocity.Z *= -deltaTime;
 	velocity.Z -= sqrt(parentCore->GetMass());
 	velocity.Z += floatiness; 
 	
 	objMesh->SetPhysicsLinearVelocity(velocity);
+}
+
+EOperations ABalloon::SetSelected(const bool value)
+{
+	selected = value;
+
+	// Only use continuous collisions while selected (to prevent objects from going through objects).
+	objMesh->SetUseCCD(selected);
+	
+	ToggleGravity();
+	SetHideIndicator(!selected);
+
+	if(selected)
+	{
+		wasDetached = isAttached;
+		Detach();
+		
+		canPlace = true;
+		return {EOperations::Detach};
+	}
+
+	if(!parentCore) return { wasDetached? EOperations::Detach : EOperations::Move};
+	if(attachedSocket == NAME_None) return { wasDetached? EOperations::Detach : EOperations::Move};
+
+	if(!previousObj) previousObj = parentCore;
+	
+	Attach();
+	UseSilhouetteTransform();
+	ResetGhost();
+
+	parentCore->AddAttachment(this, attachedSocket);
+	isAttached = true;
+	
+	return {EOperations::Attach};
+}
+
+void ABalloon::Detach()
+{
+	ResetGhost();
+
+	if(!parentCore && !previousObj)
+	{
+		Print("Couldn't detach... parent was invalid..", 4)
+		return;
+	}
+
+	SetAbilityActive(false);
+
+	ResetMaterial();
+	
+	if(parentCore) parentCore->RemoveAttachment(attachedSocket);
+	else previousObj->RemoveAttachment(attachedSocket);
+
+	constraint->BreakConstraint();
+	string->SetAttachEndToComponent(nullptr);
+	
+	silhouette->SetupAttachment(objMesh);
+
+	if(parentCore)
+	{
+		previousObj = parentCore;
+		parentCore = nullptr;
+	}
+
+	ToggleGravity(true);
+	isAttached = false;
+}
+
+void ABalloon::Attach() const
+{
+	UStaticMeshComponent* parentMesh = parentCore->GetMesh();
+	constraint->SetConstrainedComponents(parentMesh,"", objMesh, "");
+	string->SetAttachEndToComponent(parentMesh, attachedSocket);
 }
--- projProblox/Source/projProblox/Pickupables/Balloon.h
+++ projProblox/Source/projProblox/Pickupables/Balloon.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "CableComponent.h"
 #include "PickupableMaster.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
 #include "Balloon.generated.h"
 
 /**
@@ -13,11 +15,29 @@ UCLASS()
 class PROJPROBLOX_API ABalloon : public APickupableMaster
 {
 	GENERATED_BODY()
-
+	ABalloon();
+	
 	virtual void BeginPlay() override { Super::BeginPlay(); wrld = GetWorld(); }
 	virtual void Tick(float DeltaSeconds) override;
-
+	
+	virtual void Ability(float deltaTime) override;
+	virtual EOperations SetSelected(const bool value) override;
+	virtual void Detach() override;
+	void SetConstraintsActive(const bool constrained) const
+	{
+		// Need to deactivate first before disabling physics
+		constraint->SetActive(constrained);
+		objMesh->SetSimulatePhysics(constrained);
+	}
+	void Attach() const;
+	
 	UWorld* wrld;
+
+	UPROPERTY(EditDefaultsOnly)
+	UPhysicsConstraintComponent* constraint;
+
+	UPROPERTY(EditDefaultsOnly)
+	UCableComponent* string;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Ability", meta = (Delta = .1f, ToolTip = "How far the flatHead should extend from the piston shaft."))
 	float floatiness = 67;
