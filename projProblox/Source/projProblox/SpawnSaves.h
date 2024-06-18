90a98daafb006b54c3e43cc0e41218a52d448c73
keddah <deanatkinsonwalker@gmail.com>
2024-06-18 10:52:33 +0100

    buid area ui

---
+++ projProblox/Source/projProblox/SpawnSaves.h
@@ -0,0 +1,17 @@
+// Created by Dean Atkinson-Walker 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "SpawnSaves.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJPROBLOX_API USpawnSaves : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
