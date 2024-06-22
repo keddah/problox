e42b4186a2f10a8cb83dad915071983b9a6e4e48
Tyler830 <139144512+Tyler830@users.noreply.github.com>
2024-06-22 21:09:39 +0100

    Move some stuff in kitchen level
    moved spawn points out of level and added the trigger to everything in level

--- projProblox/Intermediate/Build/Win64/UnrealEditor/Inc/projProblox/UHT/LevelManager.gen.cpp
+++ projProblox/Intermediate/Build/Win64/UnrealEditor/Inc/projProblox/UHT/LevelManager.gen.cpp
@@ -666,15 +666,15 @@ void FOnChangedLevels_DelegateWrapper(const FMulticastScriptDelegate& OnChangedL
 	}
 	DEFINE_VTABLE_PTR_HELPER_CTOR(ALevelManager);
 	ALevelManager::~ALevelManager() {}
-	struct Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_LevelManager_h_Statics
+	struct Z_CompiledInDeferFile_FID_projProblox_Source_projProblox_LevelManager_h_Statics
 	{
 		static const FClassRegisterCompiledInInfo ClassInfo[];
 	};
-	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_LevelManager_h_Statics::ClassInfo[] = {
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_projProblox_Source_projProblox_LevelManager_h_Statics::ClassInfo[] = {
 		{ Z_Construct_UClass_ALevelManager, ALevelManager::StaticClass, TEXT("ALevelManager"), &Z_Registration_Info_UClass_ALevelManager, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(ALevelManager), 1738238460U) },
 	};
-	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_LevelManager_h_2274800306(TEXT("/Script/projProblox"),
-		Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_LevelManager_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_Deany_Documents_GitHub_problox_projProblox_Source_projProblox_LevelManager_h_Statics::ClassInfo),
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_projProblox_Source_projProblox_LevelManager_h_2274800306(TEXT("/Script/projProblox"),
+		Z_CompiledInDeferFile_FID_projProblox_Source_projProblox_LevelManager_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_projProblox_Source_projProblox_LevelManager_h_Statics::ClassInfo),
 		nullptr, 0,
 		nullptr, 0);
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
