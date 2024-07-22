c7f9c1276d014677b40d7b4d92827247e8484420
keddah <deanatkinsonwalker@gmail.com>
2024-07-22 19:13:12 +0100

    fixed balloons breaking on respawn

--- projProblox/Intermediate/Build/BuildRulesProjects/projProbloxModuleRules/obj/Development/projProbloxModuleRules.AssemblyInfo.cs
+++ projProblox/Intermediate/Build/BuildRulesProjects/projProbloxModuleRules/obj/Development/projProbloxModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projProbloxModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6a830e76dc23e694f2a8d14ba3a76bc93ab444e")]
 [assembly: System.Reflection.AssemblyProductAttribute("projProbloxModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projProbloxModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- projProblox/Source/projProblox/Pickupables/Cores/CubeCore.cpp
+++ projProblox/Source/projProblox/Pickupables/Cores/CubeCore.cpp
@@ -227,8 +227,9 @@ void ACubeCore::Teleport(const FRotator& rot, const FVector& pos = FVector(), bo
 	{
 		if(obj->IsA<ABalloon>())
 		{
-			// Using != since this gets called whilst still in the level you're changing from
-			obj->GetMesh()->SetSimulatePhysics(currentMode != EGameMode::Story);
+			if(!IsValid(instance)) return;
+			
+			obj->GetMesh()->SetSimulatePhysics(instance->GetCurrentLevel() != 0);
 			
 			// Since balloons aren't actually attached to the core....
 			obj->SetActorLocation(lastSpawnPos + FVector(0,0,100));
